3ad0e25de72dea53391513666a5cce46fddf9e77
FinalLabs <63569156+FinalLabs@users.noreply.github.com>
2021-01-27 17:54:32 -0800

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,11 @@
+function downloadRedirect() {
+    window.location.replace("downloads.html");
+}
+
+function indexRedir() {
+    window.location.replace("index.html"); //put discord url there 
+}
+
+function discordRedirect() {
+    window.location.replace(""); //put discord url there 
+}
